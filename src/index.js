83a6703db265eaf93d24c8d6182560216c57cd13
wangbo <781278657@qq.com>
2021-10-22 15:39:12 +0800

    add: 完成数组的转换器

--- src/index.js
+++ src/index.js
@@ -34,31 +34,34 @@ class Transform {
     /**
      *
      * @param dataInterface 数据类型：支持验证 String Number Boolean Array Object 格式数据
-     * @param data 需要转换数据：传入的数据仅支持对象和数组
+     * @param rawData 需要转换数据：传入的数据仅支持对象和数组
      * @param defaultData 默认数据，转换数据的属性类型校验不通过时，赋的默认值
      */
-    constructor(dataInterface, data, defaultData) {
-        Object.assign(this, {dataInterface, data, defaultData})
+    constructor(dataInterface, rawData, defaultData) {
+        Object.assign(this, {dataInterface, rawData, defaultData})
         this.setInitResult()
-        this.validator()
     }
 
     setInitResult() {
-        if (Array.isArray(this.data)) {
+        const {rawData} = this
+        if (Array.isArray(rawData)) {
             this.result = []
-        } else if (_.isObject(this.data)) {
+            this.arrayValidator()
+        } else if (_.isObject(rawData)) {
             this.result = {}
+            this.validator(rawData, this.result)
         } else {
-            throw new Error(`The parameters passed in are limited to objects or arrays! But got the ${_.getType(this.data)} ${this.data}.`)
+            throw new Error(`The parameters passed in are limited to objects or arrays! But got the ${_.getType(this.rawData)} ${this.rawData}.`)
         }
     }
 
-    validator() {
-        const {dataInterface, data, result, defaultData} = this
+    validator(rawData, result) {
+        const {dataInterface, defaultData} = this
         Object.keys(dataInterface).forEach(key => {
             // 预设类型
             const presetType = dataInterface[key].name
-            const value = data[key]
+            // 被转换数据的值
+            const value = rawData[key]
             // 实际类型
             const dataType = _.getType(value)
             // 预设类型和实际类型一样则直接赋值
@@ -75,6 +78,14 @@ class Transform {
                     result[key] = PRESET_DATA[presetType]
                 }
             }
+        })
+    }
+
+    arrayValidator() {
+        const {rawData, result} = this
+        rawData.forEach((item, index) => {
+            result.push({})
+            this.validator(item, result[index])
         })
     }
 
