d71da7f79c0cd2816151bbbe4a89d9379738ba1e
wangbo <781278657@qq.com>
2021-10-22 10:49:08 +0800

    add: 基础功能实现

--- src/index.js
+++ src/index.js
@@ -1,38 +1,44 @@
 const _ = {
-    String(val) {
-        return Object.prototype.toString.call(val) === '[object String]'
+    getType(val) {
+        return Object.prototype.toString.call(val)
     },
-    Number(val) {
-        return Object.prototype.toString.call(val) === '[object Number]'
+    getShortType(type) {
+        return type.slice(1, -1).split(' ')[1]
     },
-    Boolean(val) {
-        return Object.prototype.toString.call(val) === '[object Boolean]'
+    isString(val) {
+        return this.getType(val) === '[object String]'
     },
-    Array(val) {
+    isNumber(val) {
+        return this.getType(val) === '[object Number]'
+    },
+    isBoolean(val) {
+        return this.getType(val) === '[object Boolean]'
+    },
+    isArray(val) {
         return Array.isArray(val)
     },
-    Object(val) {
-        return Object.prototype.toString.call(val) === '[object Object]'
-    },
-    Date(val) {
-        return Object.prototype.toString.call(val) === '[object Date]'
-    },
-    Function(val) {
-        return Object.prototype.toString.call(val) === '[object Function]'
-    },
-    Symbol(val) {
-        return Object.prototype.toString.call(val) === '[object Symbol]'
+    isObject(val) {
+        return this.getType(val) === '[object Object]'
     }
+}
+
+const PRESET_DATA = {
+    String: '-',
+    Number: 0,
+    Boolean: false,
+    Array: [],
+    Object: {}
 }
 
 class Transform {
     /**
      *
-     * @param dataInterface 数据类型：支持验证 String Number Boolean Array Object Date Function Symbol 格式数据
-     * @param data 数据：传入的数据仅支持对象和数组
+     * @param dataInterface 数据类型：支持验证 String Number Boolean Array Object 格式数据
+     * @param data 需要转换数据：传入的数据仅支持对象和数组
+     * @param defaultData 默认数据，转换数据的属性类型校验不通过时，赋的默认值
      */
-    constructor(dataInterface, data) {
-        Object.assign(this, {dataInterface, data})
+    constructor(dataInterface, data, defaultData) {
+        Object.assign(this, {dataInterface, data, defaultData})
         this.setInitResult()
         this.validator()
     }
@@ -40,18 +46,36 @@ class Transform {
     setInitResult() {
         if (Array.isArray(this.data)) {
             this.result = []
-        } else if (_.Object(this.data)) {
+        } else if (_.isObject(this.data)) {
             this.result = {}
         } else {
-            throw new Error('传入的参数约束为对象和数组！')
+            throw new Error(`The parameters passed in are limited to objects or arrays! But got the ${_.getType(this.data)} ${this.data}.`)
         }
     }
 
     validator() {
-        const {dataInterface, data} = this
+        const {dataInterface, data, result, defaultData} = this
         Object.keys(dataInterface).forEach(key => {
-            const dataType = dataInterface[key].name
-
+            // 预设类型
+            const presetType = dataInterface[key].name
+            const value = data[key]
+            // 实际类型
+            const dataType = _.getType(value)
+            console.log(presetType, dataType);
+            // 预设类型和实际类型一样则直接赋值
+            if (dataType.includes(presetType)) {
+                result[key] = value
+            } else {
+                const defaultVal = defaultData && defaultData[key]
+                // 不一样给出警告，有默认值赋
+                console.warn(`${key} is preset to ${presetType}, but got the ${dataType}`)
+                if (defaultVal) {
+                    result[key] = defaultVal
+                } else {
+                    // 没有赋预设值
+                    result[key] = PRESET_DATA[presetType]
+                }
+            }
         })
     }
 
