0a1eddd845245edbf7af76fae9c5686cfe4a4d0b
wangbo <781278657@qq.com>
2021-10-26 14:47:26 +0800

    update: 优化

--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,89 @@
 import * as _ from './utils'
-import {PRESET_DATA} from './utils'
+import { PRESET_DATA, PresetDataKeyType } from './utils'
+
+type dataType = string | number | boolean | Array<never> | Record<string, unknown> | undefined
 
 interface IObject {
-    [propName: string]: dataType;
+  [propName: string]: dataType;
 }
+
+type TypeConstructor = StringConstructor | NumberConstructor | BooleanConstructor | ArrayConstructor | ObjectConstructor
 
 interface IValidator {
-    [propName: string]: { new(...args: any[]): Function };
+  [propName: string]: TypeConstructor;
 }
 
-type resultType = IObject | any[]
-
-type dataType = string | number | boolean | Array<any> | IObject
+type resultType = IObject | IObject[]
 
 export default class Transform {
-    private result: resultType
-    private dataInterface: IValidator
-    private rawData: resultType
-    private defaultData: resultType
+  private readonly result: resultType
+  private readonly dataInterface: IValidator
+  private readonly rawData: resultType
+  private readonly defaultData: IObject | undefined
 
-    constructor(dataInterface: IValidator, rawData: resultType, defaultData?: IObject) {
-        Object.assign(
-            this,
-            {
-                dataInterface,
-                rawData,
-                defaultData
-            }
-        )
-        this.setInitResult();
+  constructor (dataInterface: IValidator, rawData: resultType, defaultData?: IObject) {
+    this.dataInterface = dataInterface
+    this.rawData = rawData
+    this.defaultData = defaultData
+
+    if (Array.isArray(rawData)) {
+      this.result = []
+      this.arrayValidator()
+    } else if (_.isObject(rawData)) {
+      this.result = {}
+      this.validator(
+        rawData,
+        this.result
+      )
+    } else {
+      throw new Error(`The parameters passed in are limited to objects or arrays! But got the ${_.getType(this.rawData)} ${this.rawData}.`)
     }
+  }
 
-    setInitResult(): void {
-
-        const {rawData} = this;
-        if (Array.isArray(rawData)) {
-
-            this.result = [];
-            this.arrayValidator();
-
-        } else if (_.isObject(rawData)) {
-
-            this.result = {};
-            this.validator(
-                rawData,
-                this.result
-            );
-
+  validator (rawData: IObject, result: IObject): void {
+    const {
+      dataInterface,
+      defaultData
+    } = this
+    Object.keys(dataInterface).forEach((key) => {
+      // 预设类型
+      const presetType: string = dataInterface[key].name
+      // 被转换数据的值
+      const value: dataType = rawData[key]
+      // 实际类型
+      const dataType: string = _.getType(value)
+      // 预设类型和实际类型一样则直接赋值
+      if (dataType.includes(presetType)) {
+        result[key] = value
+      } else {
+        const defaultVal: dataType = defaultData && defaultData[key]
+        // 不一样给出警告，有默认值赋值
+        console.warn(`${key} is preset to ${presetType}, but got the ${dataType}`)
+        if (defaultVal) {
+          result[key] = defaultVal
         } else {
-
-            throw new Error(`The parameters passed in are limited to objects or arrays! But got the ${_.getType(this.rawData)} ${this.rawData}.`);
-
+          // 没有赋预设值值
+          result[key] = PRESET_DATA[(presetType as PresetDataKeyType)]
         }
+      }
+    })
+  }
 
-    }
+  arrayValidator (): void {
+    const {
+      rawData,
+      result
+    } = this;
+    (rawData as Array<IObject>).forEach((item, index) => {
+      (result as Array<IObject>).push({})
+      this.validator(
+        item,
+        (result as Array<IObject>)[index]
+      )
+    })
+  }
 
-    validator(rawData: resultType, result: resultType) {
-
-        const {dataInterface, defaultData} = this;
-        Object.keys(dataInterface).forEach((key) => {
-
-            // 预设类型
-            const presetType: string = dataInterface[key]['name'],
-                // 被转换数据的值
-                value: dataType = rawData[key],
-                // 实际类型
-                dataType: string = _.getType(value);
-            // 预设类型和实际类型一样则直接赋值
-            if (dataType.includes(presetType)) {
-
-                result[key] = value;
-
-            } else {
-
-                const defaultVal: dataType = defaultData && defaultData[key];
-                // 不一样给出警告，有默认值赋值
-                console.warn(`${key} is preset to ${presetType}, but got the ${dataType}`);
-                if (defaultVal) {
-
-                    result[key] = defaultVal;
-
-                } else {
-
-                    // 没有赋预设值值
-                    result[key] = PRESET_DATA[presetType];
-
-                }
-
-            }
-
-        });
-
-    }
-
-    arrayValidator(): void {
-
-        const {rawData, result} = this;
-        (rawData as Array<IObject>).forEach((item, index) => {
-
-            (result as Array<IObject>).push({});
-            this.validator(
-                item,
-                result[index]
-            );
-
-        });
-
-    }
-
-    getData(): resultType {
-
-        return this.result;
-
-    }
+  getData (): resultType {
+    return this.result
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,31 @@
-export const getType = (val: any): string => Object.prototype.toString.call(val)
+export const getType = (val: unknown): string => Object.prototype.toString.call(val)
 
-export const getShortType = (type: string): string => type.slice(1, -1).split(" ")[1]
+export const getShortType = (type: string): string => type.slice(1, -1).split(' ')[1]
 
-export const isString = (val: any): boolean => getType(val) === "[object String]"
+export const isString = (val: unknown): boolean => getType(val) === '[object String]'
 
-export const isNumber = (val: any): boolean => getType(val) === "[object Number]"
+export const isNumber = (val: unknown): boolean => getType(val) === '[object Number]'
 
-export const isBoolean = (val: any): boolean => getType(val) === "[object Boolean]"
+export const isBoolean = (val: unknown): boolean => getType(val) === '[object Boolean]'
 
-export const isArray = (val: any): boolean => Array.isArray(val)
+export const isArray = (val: unknown): boolean => Array.isArray(val)
 
-export const isObject = (val: any): boolean => getType(val) === "[object Object]"
+export const isObject = (val: unknown): boolean => getType(val) === '[object Object]'
 
-interface PresetData {
-    String: string
-    Number: number
-    Boolean: boolean
-    Array: any[]
-    Object: any
+export interface PresetData {
+  String: string
+  Number: number
+  Boolean: boolean
+  Array: never[]
+  Object: Record<string, unknown>
 }
 
+export type PresetDataKeyType = 'String' | 'Number' | 'Boolean' | 'Array' | 'Object'
+
 export const PRESET_DATA: PresetData = {
-    "String": "-",
-    "Number": 0,
-    "Boolean": false,
-    "Array": [],
-    "Object": {}
+  String: '-',
+  Number: 0,
+  Boolean: false,
+  Array: [],
+  Object: {}
 }
